86c7bd95d83a4e1b5e9323d887830ce9abaf2679
Hesam Asnaashari <ilhesam@outlook.com>
2020-03-08 03:41:33 +0330

    Add project files.

---
+++ ExchangeSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExchangeSort
+{
+    class Program
+    {
+        // Exchange Sort with Random Numbers
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+
+            var sortedArray = ExchangeSort(array);
+
+            foreach (var num in sortedArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int[] ExchangeSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        int container = array[j];
+                        array[j] = array[i];
+                        array[i] = container;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
